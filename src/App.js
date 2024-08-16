602a7fd9753f22bc324985261a86dc5563bc1839
Dusty Shaw <shustydaw@gmail.com>
2024-08-16 12:42:58 -0600

    Board now handles useState

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import './App.css';
 import { useState } from 'react';
 
-function Square() {
-
-  const [squareVal, setValue] = useState(null);
-
-  function HandleClick() {
-    setValue('X');
-  }
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={HandleClick}>{squareVal}</button>
+    <button className="square" onClick={onSquareClick}>{value}</button>
   );
 }
 
 export default function Board () {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();  // Create a copy of the squares array
+    nextSquares[i] = "X"; 
+    setSquares(nextSquares);  // lets React know the state of component has changed
+  }
+
   return (
     <>
       <div>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
       </div>
       <div>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
       </div>
       <div>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
       </div>
     </>
 );
